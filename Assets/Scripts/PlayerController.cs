c3ec8b722d5d1826b3c4372430cd9e6182140228
Cesar Sanchez <c_sanchez21@hotmail.com>
2017-02-23 18:05:25 -0800

    Added Movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    #region Fields
+    private Rigidbody rb;
+    public float Speed;
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+    //Called before rendering a frame
+	void Update () {
+		
+	}
+
+    //Called before peformaning any physics calculations
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement*Speed);
+    }
+}

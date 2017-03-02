4c5cd335a164ef6f9900acddae4166758c65f756
Cesar Sanchez <c_sanchez21@hotmail.com>
2017-03-01 21:52:22 -0800

    Added Pick Up Behavior

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -28,4 +28,10 @@ public class PlayerController : MonoBehaviour {
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         rb.AddForce(movement*Speed);
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+            other.gameObject.SetActive(false);
+    }
 }
